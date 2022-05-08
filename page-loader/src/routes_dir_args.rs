04763664d67cb87744b19bdb45af7e8401657256
Lien <cantorne.neil@gmail.com>
2022-05-08 09:39:26 +0800

    implement iterator

--- page-loader/src/routes_dir_args.rs
+++ page-loader/src/routes_dir_args.rs
@@ -1,8 +1,11 @@
 use std::{env::var, path::PathBuf};
 
-#[derive(Debug)]
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+
+#[derive(Debug, Clone)]
 pub(crate) struct RouteDirectoryArgs {
-    pub(super) actix_app: syn::Path,
+    pub(super) actix_app: TokenStream,
     pub(super) directory: PathBuf
 }
 
@@ -21,17 +24,57 @@ impl RouteDirectoryArgs {
 
         Err(syn::Error::new_spanned(dir_str, "Directory not found"))
     }
+
+    pub(crate) fn route_item(&self) -> RouteItems {
+        let dir_iter = self.directory
+            .read_dir()
+            .expect("Unable to read directory");
+
+            RouteItems {
+                parent: self.directory.clone(),
+                inner: dir_iter
+            }    
+    }
 }
 
 impl syn::parse::Parse for RouteDirectoryArgs {
     fn parse(input: syn::parse::ParseStream) -> Result<Self, syn::Error> {
-        let actix_app =  input.parse::<syn::Path>()?;
+        let actix_app =  input.parse::<syn::Ident>()?.to_token_stream();
         input.parse::<syn::Token!(,)>()?;
+
         let directory: PathBuf = Self::parse_directory(input.parse()?)?;
 
         Ok(Self {
             actix_app,
             directory
         })
+    }
+}
+
+pub (crate) struct RouteItems {
+    pub(self) parent: PathBuf,
+    pub(self) inner: std::fs::ReadDir
+} 
+
+impl Iterator for RouteItems  {
+    type Item =  (String, String);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(result) = self.inner.next() {
+            if let Ok(entry) = result {
+                if let Ok(rel_path)  = entry.path()
+                .strip_prefix(self.parent.as_path()) {
+                    return Some((
+                        format!("/{}", rel_path.to_str().unwrap()),
+                        entry.path()
+                                .to_str()
+                                .unwrap()
+                                .to_owned()
+                    ));
+                }
+            }
+        }
+
+        None
     }
 }
