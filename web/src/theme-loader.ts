c5ce70da37a1a0308c212221029628b7f2237384
Lien <cantorne.neil@gmail.com>
2022-05-08 18:15:43 +0800

    add theme loader

---
+++ web/src/theme-loader.ts
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', '/theme/midnight');
+    document.head.appendChild(link);
+})
