ae7f2c131712794e59643f8e35792dab53b96a5d
Lien <cantorne.neil@gmail.com>
2022-05-09 01:27:00 +0800

    implement DOM generation

--- web/components/dock-panel.ts
+++ web/components/dock-panel.ts
@@ -1,3 +1,6 @@
+import { DockButtonElement } from './dock-button';
+
+export { DockButtonElement };
 
 // @ts-ignore
 import * as style from './dock-panel.scss'; style;
@@ -7,15 +10,44 @@ export class DockPanelElement extends HTMLElement {
 
     constructor() {
         super();
+        this.generateDOM();
+    }
 
+    private generateDOM() {
         this.shadow = this.attachShadow({mode: 'closed'})
-        
-        Array.from(this.children).forEach((element) => {
-            //todo: build
+
+        // Add item to DOM
+        Array.from(this.children).forEach((node) => {
+            if(node instanceof DockButtonElement) {
+                this.shadow.appendChild(node.model);
+            }
         })
+        
+        // Observe DOM changes
+        let observer = new MutationObserver(this.handleDOMChanged.bind(this));
+        observer.observe(this, { childList: true });
+    }
+
+    private handleDOMChanged(record: MutationRecord[]) {
+        record.forEach(mutation => {
+            // Add nodes to DOM
+            mutation.addedNodes.forEach(node => {
+                if(node instanceof DockButtonElement) {
+                    this.shadow.appendChild(node.model);
+                }
+            })
+
+            // Remove nodes from DOM
+            mutation.removedNodes.forEach(node => {
+                if(node instanceof DockButtonElement) {
+                    this.shadow.removeChild(node.model)
+                }
+            })
+        });
     }
 
     static register() {
+        customElements.define('dock-button', DockButtonElement);
         customElements.define('dock-panel', DockPanelElement);
     }
 }
